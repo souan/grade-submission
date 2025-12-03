bae246a83fe2296e4aadd50f514740675789c4f5
André <andre.silva@education.lu>
2025-12-03 13:14:03 +0100

    first commit

---
+++ src/main/java/lu/cnfpc/grade_submission/GradeSubmissionApplication.java
@@ -0,0 +1,13 @@
+package lu.cnfpc.grade_submission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeSubmissionApplication.class, args);
+	}
+
+}
