17c1e30b2ee440f44a62e4230ad152ceccd36d97
André <andre.silva@education.lu>
2025-12-05 12:40:46 +0100

    Utility Methods and Loops

--- src/main/java/lu/cnfpc/grade_submission/GradeController.java
+++ src/main/java/lu/cnfpc/grade_submission/GradeController.java
@@ -1,5 +1,9 @@
 package lu.cnfpc.grade_submission;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +12,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GradeController {
 
+    //ArrayList of grade POJO objects
+    List<Grade> studentGrades = Arrays.asList(
+        new Grade("Victor","Mathematics","15"),
+        new Grade("Bernard","German","10"),
+        new Grade("Jean","French","9")
+    );
+
     @GetMapping("/grades")
     //handler method getGrades
     public String getGrades(Model model) {
-        Grade grade = new Grade("Victor","Mathematics","15");
         //add data to the model
-        model.addAttribute("grade", grade);
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
 
---
+++ src/main/java/lu/cnfpc/grade_submission/UtiliyMethodsController.java
@@ -0,0 +1,17 @@
+package lu.cnfpc.grade_submission;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class UtiliyMethodsController {
+    @GetMapping("/utility_method")
+    public String getUtil_Method(Model model) {
+        model.addAttribute("menu", "We sell chocolate drinks with bubbles");
+        model.addAttribute("name", "Victor");
+        return "utility_method";
+    }
+    
+}
